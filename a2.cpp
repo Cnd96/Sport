76b58c04b5c09b646abf6f2133e0039028ba9d38
Chamal De Mel <40256920+Cnd96@users.noreply.github.com>
2020-04-02 18:43:30 +0530

    3

--- a2.cpp
+++ a2.cpp
@@ -1,98 +1,41 @@
-//raja1999
-#include <bits/stdc++.h>
-#include <vector>
-#include <set>
-#include <map>
-#include <string>
-#include <cstdio>
-#include <cstdlib>
-#include <climits>
-#include <utility>
-#include <algorithm>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <iomanip> 
-//setbase - cout << setbase (16)a; cout << 100 << endl; Prints 64
-//setfill -   cout << setfill ('x') << setw (5); cout << 77 <<endl;prints xxx77
-//setprecision - cout << setprecision (14) << f << endl; Prints x.xxxx
-//cout.precision(x)  cout<<fixed<<val;  // prints x digits after decimal in val
-
-using namespace std;
-#define f(i,a,b) for(i=a;i<b;i++)
-#define rep(i,n) f(i,0,n)
-#define fd(i,a,b) for(i=a;i>=b;i--)
-#define pb push_back
-#define mp make_pair
-#define vi vector< int >
-#define vl vector< ll >
-#define ss second
-#define ff first
+#include<bits/stdc++.h>
 #define ll long long
-#define pii pair< int,int >
-#define pll pair< ll,ll >
-#define sz(a) a.size()
-#define inf (1000*1000*1000+5)
-#define all(a) a.begin(),a.end()
-#define tri pair<int,pii>
-#define vii vector<pii>
-#define vll vector<pll>
-#define viii vector<tri>
-#define mod (1000*1000*1000+7)
-#define pqueue priority_queue< int >
-#define pdqueue priority_queue< int,vi ,greater< int > >
-
-//std::ios::sync_with_stdio(false);
-int cnt[35],arr[100004];
+#define mod 1000000007
+#define pb push_back
+#define fi first
+#define se second
+#define fr(i,s,e) for(i=s;i<e;i++)
+#define ms(arr,val) memset(arr,val,sizeof(arr))
+using namespace std;
+const int mxn=1e5+1;
+int dp[100][100001],n;
+int arr[100];
+int func(int i,int sum)
+{
+    if(i<0)
+        return 0;
+   if(dp[i][sum]!=-1)
+       return dp[i][sum];
+    int ans=0;
+    if(sum<=6*arr[i])
+        ans=1+func(i-1,sum+arr[i]);
+    ans=max(ans,func(i-1,sum));
+    return dp[i][sum]=ans;
+}
 int main(){
-	std::ios::sync_with_stdio(false);
-	int t,t1;
-	cin>>t;
-	t1=t;
-	while(t--){
-		int n,m,p,i,j,val,ans=0,res=inf;
-		string s;
-		cin>>n>>m>>p;
-		rep(i,30){
-			cnt[i]=0;
-		}
-		rep(i,5005){
-			arr[i]=0;
-		}
-		rep(i,n){
-			cin>>s;
-			rep(j,s.length()){
-				cnt[j]+=s[j]-'0';
-			}
-		}
-		rep(i,m){
-			cin>>s;
-			val=0;
-			rep(j,p){
-				if(s[j]=='1'){
-					val+=(1<<j);
-				}
-			}
-			arr[val]=1;
-		}
-		val=(1<<p);
-		rep(i,val){
-			if(arr[i]==1){
-				continue;
-			}
-			ans=0;
-			rep(j,p){
-				if((1<<j)&i){
-					ans+=n-cnt[j];
-				}
-				else{
-					ans+=cnt[j];
-				}
-			}
-			//cout<<ans<<" "<<i<<endl;
-			res=min(res,ans);
-		}
-		cout<<"Case #"<<t1-t<<": "<<res<<endl;
-	}	
-} 
-	
+//    freopen("A.in","r",stdin);
+//    freopen("sol.out","w",stdout);
+  int t=1,tt=1;
+  cin>>t;
+    while(t--){
+        ms(dp,-1);
+        int i;
+        cin>>n;
+        fr(i,0,n)
+        cin>>arr[i];
+        reverse(arr,arr+n);
+
+        cout<<"Case #"<<tt++<<": "<<func(n-1,0)<<"\n";
+    }
+  return 0;
+  }
